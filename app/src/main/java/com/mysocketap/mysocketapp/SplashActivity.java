4658f325f50d1f42af3b1efa1fe943656094e755
Tshepo Mahlaula <tshepo.mahlaula@vcontractor.co.za>
2017-10-15 00:23:21 +0200

    Created splash screen

--- app/src/main/java/com/mysocketap/mysocketapp/SplashActivity.java
+++ app/src/main/java/com/mysocketap/mysocketapp/SplashActivity.java
@@ -6,12 +6,11 @@ import android.os.Bundle;
 import com.mysocketap.mysocketapp.views.SocetConnectionView;
 import com.mysocketap.mysocketapp.views.SocketConnectionActivity;
 
-public class MainActivity extends AppCompatActivity implements SocetConnectionView {
+public class SplashActivity extends AppCompatActivity implements SocetConnectionView {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         Intent i = new Intent(this, SocketConnectionActivity.class);
         startActivity(i);
